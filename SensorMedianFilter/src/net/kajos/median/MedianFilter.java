0880904e957dc74b828af64656bfc590447fee9d
Stephen L <lrq3000@gmail.com>
2015-09-15 19:17:04 +0200

    add 3 more filter types (mean, lowpass, additive smoothing)
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- SensorMedianFilter/src/net/kajos/median/MedianFilter.java
+++ SensorMedianFilter/src/net/kajos/median/MedianFilter.java
@@ -37,14 +37,21 @@ public class MedianFilter implements IXposedHookLoadPackage {
 		            	// Init the variables
                         float medianValues[][] = new float[3][filter_size]; // stores the last sensor's values in each dimension (3D so 3 dimensions)
                         float tmpArray[] = new float[medianValues[0].length]; // used to temporarily copy medianValues to compute the median
-                        //float prevValues[] = new float[3]; // stores the previous sensor's values to restore them if needed
+                        float prevValues[] = new float[3]; // stores the previous sensor's values to restore them if needed
 
                         private void changeSensorEvent(float[] values) {
+                        	// Note about values[]:
+                        	// values[] contains the current sensor's value for each axis (there are 3 since it's in 3D).
+                        	// The values are measured in rad/s, which is standard since Android 2.3 (before, some phones can return values in deg/s).
+                        	// To get values of about 1.0 you have to turn at a speed of almost 60 deg/s.
+
                         	// Get preferences
                         	pref.makeWorldReadable(); // try to make the preferences world readable (because here we are inside the hook, we are not in our app anymore, so normally we do not have the rights to access the preferences of our app)
                         	pref.reload(); // reload the preferences to get the latest value (ie, if the user changed the values without rebooting the phone)
                         	int filter_size_new = Integer.parseInt(pref.getString("filter_size", "10")); // number of sample values to keep to compute the median
                         	float filter_min_change_new = Float.parseFloat(pref.getString("filter_min_change", "0.0")); // minimum value change threshold in sensor's value to aknowledge the new value (otherwise don't change the value, but we still register the new value in the median array)
+                        	String filter_type = pref.getString("filter_type", "median");
+                        	float filter_alpha = Float.parseFloat(pref.getString("filter_alpha", "-1"));
 
                         	// Copy new value of min change threshold
                         	filter_min_change = filter_min_change_new;
@@ -80,30 +87,57 @@ public class MedianFilter implements IXposedHookLoadPackage {
                                 medianValues[k][0] = values[k];
 
                                 // -- Compute the median and replace the current gyroscope's value
-                                // Copy the values to a temporary array
-                                for (int f = 0; f < tmpArray.length; f++) {
-                                    tmpArray[f] = medianValues[k][f];
+
+                                float median = 0.0f;
+                                if (filter_type.equals("median")) {
+	                                // Median
+	                                // Copy the values to a temporary array
+	                                for (int f = 0; f < tmpArray.length; f++) {
+	                                    tmpArray[f] = medianValues[k][f];
+	                                }
+	
+	                                Arrays.sort(tmpArray); // Sort the values
+	
+	                                // Pick the median value
+	                                median = tmpArray[(int)(tmpArray.length/2)];
+
+                                } else if (filter_type.equals("mean")) {
+	                                // Moving average (special reduced case of a low-pass filter)
+	                                float sum = 0.0f;
+	                                for (float val : medianValues[k]) sum += val;
+	                                median = sum / medianValues[k].length;
+
+                                } else if (filter_type.equals("lowpass")) {
+	                                // Low pass filter
+	                                float alpha = 0.2f;
+	                                if (filter_alpha >= 0.0f) alpha = filter_alpha;
+	                                median = lowPass(alpha, values[k], prevValues[k]);
+
+                                } else if (filter_type.equals("addsmooth")) {
+	                            	// Additive smoothing
+	                                float alpha = 0.1f;
+	                                if (filter_alpha >= 0.0f) alpha = filter_alpha;
+	                                float sum = 0.0f;
+	                                for (float val : medianValues[k]) sum += val;
+	                                median = (values[k] + alpha) / (sum + alpha*medianValues.length);
                                 }
-
-                                // Sort the values
-                                Arrays.sort(tmpArray);
-
-                                // Pick the median value
-                                float median = tmpArray[(int)(tmpArray.length/2)];
 
                                 // Update the sensor's value for each axis
-                                if (filter_min_change <= 0.0f || // either filter min change threshold is disabled (value == 0)
-                                		Math.abs(values[k] - median) >= filter_min_change) { // or it is enabled (value > 0) and then we check if the current median difference with the previous sensor's value is above the minimum change threshold
-	                                // Set median in place of the value for this sensor's axis
-                                	Log.d("MedianFilter", "MedianFilter moving axis: "+k+" median: "+Float.toString(median)+" current_val:"+Float.toString(values[k])+" previous_val:"+Float.toString(medianValues[k][1]));
-	                                values[k] = median;
-                                } else {
-                                	// else do not move this sensor's axis
-                                	Log.d("MedianFilter", "MedianFilter NOT MOVING axis: "+k+" median: "+Float.toString(median)+" current_val:"+Float.toString(values[k])+" previous_val:"+Float.toString(medianValues[k][1]));
-                                	values[k] = 0.0f; // nullify the sensor for this axis, so that it does not move
+                                if (values[k] != 0.0f) {
+	                                if (filter_min_change <= 0.0f || // either filter min change threshold is disabled (value == 0)
+	                                		Math.abs(values[k] - median) >= filter_min_change) { // or it is enabled (value > 0) and then we check if the current median difference with the previous sensor's value is above the minimum change threshold
+		                                // Set median in place of the value for this sensor's axis
+	                                	Log.d("MedianFilter", "MedianFilter moving axis: "+k+" median: "+Float.toString(median)+" current_val:"+Float.toString(values[k])+" previous_val:"+Float.toString(medianValues[k][1]));
+		                                values[k] = median;
+	                                } else {
+	                                	// else do not move this sensor's axis
+	                                	Log.d("MedianFilter", "MedianFilter NOT MOVING axis: "+k+" median: "+Float.toString(median)+" current_val:"+Float.toString(values[k])+" previous_val:"+Float.toString(medianValues[k][1]));
+	                                	values[k] = 0.0f; // nullify the sensor for this axis, so that it does not move
+	                                }
                                 }
+
                                 // Remember the current sensor's value
-                                //prevValues[k] = values[k];
+                                prevValues[k] = values[k];
                             }
                         }
 
@@ -126,5 +160,23 @@ public class MedianFilter implements IXposedHookLoadPackage {
         } catch (Throwable t) {
             // Do nothing
         }
+    }
+
+    /**
+     * Low-pass filter
+     * @see http://en.wikipedia.org/wiki/Low-pass_filter#Algorithmic_implementation
+     * @see http://en.wikipedia.org/wiki/Low-pass_filter#Simple_infinite_impulse_response_filter
+     * alpha is the time smoothing constant for low-pass filter
+     * 0 <= alpha <= 1 ; a smaller value basically means more smoothing
+     * See: http://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
+     * All credits go to Thom Nichols thom_nic  http://blog.thomnichols.org/2011/08/smoothing-sensor-data-with-a-low-pass-filter and http://stackoverflow.com/a/5780505/1121352
+     */
+    protected float lowPass(float alpha, float current, float prev) {
+        //if ( prev == null ) return current;
+
+        //for ( int i=0; i<input.length; i++ ) {
+        float output = prev + alpha * (current - prev);
+        //}
+        return output;
     }
 }
