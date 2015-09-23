9ae6515be73922e22adb066a01062a22fa1292b2
Stephen L <lrq3000@gmail.com>
2015-09-23 20:10:05 +0200

    fixed Cardboard hook
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- GyroscopeNoiseFilter/src/net/kajos/gyronoisefilter/GyroscopeNoiseFilter.java
+++ GyroscopeNoiseFilter/src/net/kajos/gyronoisefilter/GyroscopeNoiseFilter.java
@@ -178,6 +178,7 @@ public class GyroscopeNoiseFilter implements IXposedHookLoadPackage {
             			// Pre-process for this hook
 
             			// Set the number of values to anti-jitter
+            			// You should edit this for each hook
                     	int nbaxis = 3;
 
 		            	// Init the arrays
@@ -201,6 +202,9 @@ public class GyroscopeNoiseFilter implements IXposedHookLoadPackage {
                         	prevValues = (float[])retlist.get(1);
                         }
 
+                        // Hook caller
+                        // This is where we tell what we should do when the hook is triggered (ie, when the hooked function/method is called)
+                        // Basically, we just check a few stuffs about the sensor's values and then we call our changeSensorEvent() to do the rest
                         @SuppressWarnings("unchecked")
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws
@@ -264,6 +268,37 @@ public class GyroscopeNoiseFilter implements IXposedHookLoadPackage {
             XposedBridge.hookAllMethods(cla, "getEyeView", new
                     XC_MethodHook() {
 
+		    			// Pre-process for this hook
+
+		    			// Set the number of values to anti-jitter
+            			// You should edit this for each hook
+		            	int nbaxis = 12;
+
+		            	// Init the arrays
+		            	int filter_size = 10;
+		                float medianValues[][] = new float[nbaxis][filter_size]; // stores the last sensor's values in each dimension (3D so 3 dimensions)
+		                float prevValues[] = new float[nbaxis]; // stores the previous sensor's values to restore them if needed
+
+		                private void changeSensorEvent(float[] values) {
+		                	// Note about values[]:
+		                	// values[] contains the current sensor's value for each axis (there are 3 since it's in 3D).
+		                	// The values are measured in rad/s, which is standard since Android 2.3 (before, some phones can return values in deg/s).
+		                	// To get values of about 1.0 you have to turn at a speed of almost 60 deg/s.
+
+		                	// Anti-jitter the values!
+		                	// Externalizing the antiJitterValues() function (ie, putting it outside of the hook) allows us to reuse the same function for several hooks.
+		                	// However, the previous values and the history of the median values will be different for different hooks (because the values are different), so we need to preprocess the values and to store them in different arrays for each hook. That's why we do this pre-processing here (and above this function).
+		                	List<Object> retlist = antiJitterValues(values, medianValues, prevValues);
+
+		                	// Update the local arrays for this hook
+		                	medianValues = (float[][])retlist.get(0);
+		                	prevValues = (float[])retlist.get(1);
+		                }
+
+		                // Hook caller
+		                // This is where we tell what we should do when the hook is triggered (ie, when the hooked function/method is called)
+		                // Basically, we just check a few stuffs about the sensor's values and then we call our changeSensorEvent() to do the rest
+
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws
                                 Throwable {
@@ -272,8 +307,13 @@ public class GyroscopeNoiseFilter implements IXposedHookLoadPackage {
                             float[] values = (float[])param.getResult();
                             Log.d(TAG, "BABA1");
                             for (int i = 0;i<values.length;i++) {
-                            	Log.d(TAG, "BABA values: "+i+" : "+values[i]);
-                            	values[i] = 0.0f;
+                            	Log.d(TAG, "BABA before values: "+i+" : "+values[i]);
+                            	//values[i] = 0.0f;
+                            }
+                            changeSensorEvent(values);
+                            for (int i = 0;i<values.length;i++) {
+                            	Log.d(TAG, "BABA after values: "+i+" : "+values[i]);
+                            	//values[i] = 0.0f;
                             }
                             Log.d(TAG, "BABA2");
                         }
